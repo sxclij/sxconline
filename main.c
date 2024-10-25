0c0f2dff14461a2faebcb838813dec858d62ac34
sxclij <sxclij@gmail.com>
2024-10-25 15:40:47 +0000

    add bool,obj_type,f64_2,obj

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
 #include <unistd.h>
 
+
+enum bool {
+    false = 0,
+    true = 1,
+};
+enum obj_type {
+    obj_type_null,
+    obj_type_camera,
+};
+struct f64_2 {
+    double x;
+    double y;
+};
+struct obj {
+    enum obj_type type;
+    struct f64_2 pos;
+    struct obj* prev;
+    struct obj* next;
+};
+
 int main() {
     write(1, "good morning world\n", 19);
     return 0;
