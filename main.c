5037818dae4ebacb29134569d3ba8899e7e4f030
sxclij <sxclij@gmail.com>
2024-10-30 03:46:49 +0000

    world chunk block

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
-#define obj_data_size (1 << 16)
+#define world_size (1 << 14)
+#define chunk_size (1 << 8)
 #define table_type_size (1 << 8)
 #define buf_kakkokari_size (1 << 16)
 
@@ -17,6 +18,14 @@ struct i32_2 {
     int y;
 };
 struct global {
+    struct global_world {
+        struct global_chunk {
+            struct global_block {
+                enum type type;
+                char size;
+            } block[chunk_size][chunk_size];
+        } chunk[world_size / chunk_size][world_size / chunk_size];
+    } world;
     struct global_term {
         struct i32_2 ws;
         char buf[buf_kakkokari_size];
