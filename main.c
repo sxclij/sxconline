24481e32bd1f00d8ce41890d402d60fc13f6f75d
sxclij <sxclij@gmail.com>
2024-11-01 11:49:19 +0000

    block_get block_set

--- main.c
+++ main.c
@@ -46,21 +46,33 @@ struct global {
 
 static struct global global;
 
-void term_update() {
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &global.term.ws);
+struct block* block_get(int x, int y) {
+    return &global.world.chunk[x / world_size][y / world_size].block[x % world_size][y % world_size];
+}
+void block_set(int x, int y, enum type type, char size) {
+    struct block* block = block_get(x, y);
+    block->type = type;
+    block->size = size;
+}
+void term_render_dot(int x, int y) {
+    if (x < 0 || world_size <= x || y < 0 || world_size <= y) {
+        write(STDOUT_FILENO, "@", 1);
+    } else {
+        struct block* block = block_get(x, y);
+        write(STDOUT_FILENO, ".", 1);
+    }
+}
+void term_render() {
     write(STDOUT_FILENO, "\x1b[1;1H", 6);
     for (int i = 0; i < global.term.ws.ws_row; i++) {
         for (int j = 0; j < global.term.ws.ws_col; j++) {
-            int display_x = global.camera.x - (global.term.ws.ws_col / 2) + j;
-            int display_y = global.camera.y + (global.term.ws.ws_row / 2) - i;
-            if (display_x < 0 || world_size <= display_x || display_y < 0 || world_size <= display_y) {
-                write(STDOUT_FILENO, "@", 1);
-            } else {
-                char ch = 
-                write(STDOUT_FILENO, ".", 1);
-            }
+            term_render_dot(global.camera.x - (global.term.ws.ws_col / 2) + j, global.camera.y + (global.term.ws.ws_row / 2) - i);
         }
     }
+}
+void term_update() {
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &global.term.ws);
+    term_render();
 }
 void global_update() {
     term_update();
